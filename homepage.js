373e02260750e10e882875e3a5c31700f8511682
Hardik Bhimani <hardikbhimani@rocketmail.com>
2022-11-10 01:39:31 +0530

    carousel-desc-js

--- homepage.js
+++ homepage.js
@@ -2,52 +2,40 @@
 
 let next_btn = document.querySelector(".carousel-control-next");
 let prev_btn = document.querySelector(".carousel-control-prev");
+let bot_btn = document.querySelector(".carousel-indicators");
 let img_desc_block = document.querySelector(".img_description");
 
 // Event-Listeners
 
-next_btn.addEventListener("click", description_next);
-prev_btn.addEventListener("click", description_prev);
+let fp = "./txt/mathura.txt";
+window.onload = () => getting_text_file(fp);
+next_btn.addEventListener("click", common_data);
+prev_btn.addEventListener("click", common_data);
+bot_btn.addEventListener("click", common_data);
 
 // Functions
 
-window.onload = () => {
-	var client = new XMLHttpRequest();
-	client.open("GET", "./txt/mathura.txt");
-	client.onreadystatechange = () => {
-		let para = document.querySelector(".car_para");
-		let content = client.responseText;
-		para.innerHTML = content;
-	};
-	client.send();
-}
-
-function description_next() {
-	let active_img = document.querySelector(".carousel-item-next img");
+function common_data(){
+	let next_img = document.querySelector(".carousel-item-next");
+	let active_img;
+	if (next_img == null){
+		active_img = document.querySelector(".carousel-item-prev img");
+	}
+	else{
+		active_img = document.querySelector(".carousel-item-next img");
+	}
 	let alt = active_img.getAttribute("alt");
 	let txt_path = "./txt/" + alt + ".txt";
+	getting_text_file(txt_path);
+}
 
+function getting_text_file(para){
 	var client = new XMLHttpRequest();
-	client.open("GET", txt_path);
+	client.open("GET", para);
 	client.onreadystatechange = () => {
 		let para = document.querySelector(".car_para");
 		let content = client.responseText;
 		para.innerHTML = content;
 	};
 	client.send();
-}
-
-function description_prev() {
-	let active_img = document.querySelector(".carousel-item-prev img");
-	let alt = active_img.getAttribute("alt");
-	let txt_path = "./txt/" + alt + ".txt";
-
-	var client = new XMLHttpRequest();
-	client.open("GET", txt_path);
-	client.onreadystatechange = () => {
-		let para = document.querySelector(".car_para");
-		let content = client.responseText;
-		para.innerHTML = content;
-	};
-	client.send();
-}
+} 
